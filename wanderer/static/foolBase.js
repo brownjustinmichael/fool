d7af35c66ba797d128d58cb20d20217b944e3e80
Nathan Brown <nabrow03@syr.edu>
2014-10-30 13:45:28 -0700

    Hand now scrolls and resizes
    The hand will now scroll through the cards with the left and right
    arrows. The hand now resizes to fit the window size.

--- wanderer/static/foolBase.js
+++ wanderer/static/foolBase.js
@@ -1,7 +1,7 @@
-
-
 function start() {
+	roundHand();
 	cardModal();
+	handShift();
 }
 
 function cardModal() {
@@ -9,7 +9,6 @@ function cardModal() {
 		var cardName = $(this).find(".card_title h2").html();
 		var cardModifier = $(this).find(".card_level").html();
 		var cardLink = $(this).find(".play_card").html();
-		console.log(cardName)
 		$('#card_modal .card_title h2').html(cardName);
 		$('#card_modal .card_level').html(cardModifier);
 		$('#card_modal #modal_play_card').html(cardLink);
@@ -17,5 +16,41 @@ function cardModal() {
 	});
 	$("#modal_cancel").click(function(){
 		$('#card_modal').hide();
+	})
+}
+
+function roundHand(){
+	var cardWidth = $('.card_slot').width();
+	$('#card_bin').width(Math.floor($('#card_bin').width()/cardWidth)*(cardWidth + 3));
+	$('#cards').width(Math.floor($('#card_bin').width()/cardWidth)*(cardWidth + 3) + 80);
+	$('.card_button[value=back]').css('background','grey');
+	if(Math.floor($('#card_bin').width()/cardWidth) >= $('.card_slot').length){
+		$('.card_button[value=next]').css('background','grey');
+	}
+
+}
+
+function handShift(){
+	var cardWidth = $('.card_slot').width();
+	$('.card_button').click(function(){
+		var shiftCount = -(parseInt($('#card_slots').css('left'))/cardWidth);
+		var handCount = $('#card_bin').width()/cardWidth;
+		if($(this).attr('value') == 'next' && shiftCount < $('.card_slot').length - handCount){
+			$('#card_slots').animate({left:'-=' + cardWidth + 'px'}, 50);
+			shiftCount += 1;
+		}else if($(this).attr('value') == 'back' && shiftCount > 0){
+			$('#card_slots').animate({left:'+=' + cardWidth + 'px'}, 50);
+			shiftCount -= 1;
+		}
+		if(shiftCount >= $('.card_slot').length - handCount){
+			$('.card_button[value=back]').css('background','red');
+			$('.card_button[value=next]').css('background','grey');
+		}else if(shiftCount <= 0){
+			$('.card_button[value=back]').css('background','grey');
+			$('.card_button[value=next]').css('background','red');
+		}else{
+			$('.card_button[value=back]').css('background','red');
+			$('.card_button[value=next]').css('background','red');
+		}
 	})
 }
