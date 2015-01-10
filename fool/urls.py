322324770d1960941eb04d388990bdd6fae01033
Nathan Brown <nabrow03@syr.edu>
2015-01-09 16:16:10 -0800

    Fixed URLs

--- fool/urls.py
+++ fool/urls.py
@@ -8,11 +8,16 @@ urlpatterns = patterns('',
     (r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^$', 'locations.views.index'),
+
+    url(r'^exploration/home', 'locations.views.location', {"slug": 'home'}, name = "Home"),
+    url(r'^exploration/$', 'locations.views.index', name = "Subway"),
+    url(r'^journal', 'locations.views.index', name = "Journal"),
+    url(r'^user_preferences', 'locations.views.index', name = "User_Preferences"),
+                       
     url(r'^shuffle/$', 'cards.views.shuffle'),
     url(r'^draw/$', 'cards.views.draw'),
     url(r'^cards/(?P<slug>[\w\-]+)/$', 'cards.views.card'),
     url(r'^npc/(?P<slug>[\w\-]+)/$', 'npcs.views.npc'),
     url(r'^exploration/(?P<slug>[\w\-]+)/draw/$', 'locations.views.draw'),
     url(r'^exploration/(?P<slug>[\w\-]+)/$', 'locations.views.location'),
-    url(r'^exploration/', 'locations.views.index', name = "explore"),
 )
