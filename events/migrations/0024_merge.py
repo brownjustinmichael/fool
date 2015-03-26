41fb6ec4b39352912b8c0c68610a56d896fe6d14
Nathan Brown <nabrow03@syr.edu>
2015-03-26 00:56:19 -0400

    Layout Changes for Locations
    Moved locations and stack to top, moved sub locations to left.

---
+++ events/migrations/0024_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0023_auto_20150326_0132'),
+        ('events', '0023_auto_20150326_0134'),
+    ]
+
+    operations = [
+    ]
